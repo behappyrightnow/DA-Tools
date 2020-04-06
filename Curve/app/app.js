7f1572993942942d3c15976a489016fff1b01587
Somik Raha <somikraha@Antardhwani.local>
2020-04-05 23:49:08 -0700

    Refactored data into data.js and made copy to clipboard work

--- Curve/app/app.js
+++ Curve/app/app.js
@@ -11,26 +11,6 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $routeProvider.otherwise({redirectTo: '/viewDashboard'});
 }]).
 factory('setupService', function() {
-  var setupData = {
-    startDate: new Date(2020,0,21),
-    pi: Math.PI,
-    items: [
-        {"name":"100: Description of Feature 100","ten":1,"fifty":3,"ninety":5},
-        {"name":"101: Description of Feature 101","ten":5,"fifty":10,"ninety":20},
-        {"name":"102: Description of Feature 102","ten":5,"fifty":8,"ninety":15},
-        {"name":"103: Description of Feature 103","ten":3,"fifty":4,"ninety":5},
-
-        {"name":"104: Description of Feature 104","ten":2,"fifty":4,"ninety":5},
-        {"name":"105: Description of Feature 105","ten":5,"fifty":10,"ninety":20},
-        {"name":"106: Description of Feature 106","ten":4,"fifty":9,"ninety":16},
-        {"name":"107: Description of Feature 107","ten":4,"fifty":9,"ninety":16},
-        {"name":"108: Description of Feature 108","ten":4,"fifty":8,"ninety":15},
-
-        {"name":"109: Description of Feature 109","ten":2,"fifty":4,"ninety":5}
-    ],
-    units: "Team days"
-  };
-
   var getData = function() {
     return setupData;
   }
--- Curve/app/viewDashboard/viewDashboard.js
+++ Curve/app/viewDashboard/viewDashboard.js
@@ -15,6 +15,7 @@ angular.module('curve.dashboard', ['ngRoute', 'curve'])
         item["editing"] = false;
         return item;
     });
+    $scope.showPublish = false;
     var p = [0.1, 0.5, 0.9];
 
     function getMonthNameFor(d) {
@@ -272,10 +273,53 @@ angular.module('curve.dashboard', ['ngRoute', 'curve'])
     $scope.recalcDate = function() {
         $scope.sum = sum();
     }
+
+    $scope.togglePublish = function() {
+        $scope.showPublish = !$scope.showPublish;
+    }
+
+    $scope.copyToClipboard = function() {
+        copyTextToClipboard("var setupData = "+JSON.stringify($scope.data)+";");
+    }
 }]);
 
 function addDays(d, n) {
     d = new Date(d.getTime());
     d.setDate(d.getDate() + n);
     return d;
+}
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
 }
